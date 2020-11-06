f1882244b5006107c8f4edc892500672c56ebb74
marcos109 <52711893+marcos109@users.noreply.github.com>
2020-11-06 17:45:37 -0600

    Create My.php

---
+++ My.php
@@ -0,0 +1,169 @@
+<! DOCTYPE html>
+<html lang = "es">
+<cabeza>
+<meta charset = "UTF-8" />
+<meta name = "viewport" content = "width = device-width, initial-scale = 1">
+<link rel = "profile" href = "http://gmpg.org/xfn/11" />
+<link rel = "pingback" href = "http://blackaddon.com/xmlrpc.php" />
+<title> Página no encontrada - Black Addon </title>
+<link rel = 'dns-prefetch' href = '// fonts.googleapis.com' />
+<enlace rel = 'dns-prefetch' href = '// sworg' />
+<link rel = "alternate" type = "application / rss + xml" title = "Black Addon» Feed "href =" http://blackaddon.com/feed/ "/>
+<link rel = "alternate" type = "application / rss + xml" title = "Black Addon» Feed de los comentarios "href =" http://blackaddon.com/comments/feed/ "/>
+		<script type = "text / javascript">
+			window._wpemojiSettings = {"baseUrl": "https: \ / \ / sworg \ / images \ / core \ / emoji \ /12.0.0-1 \ / 72x72 \ /", "ext": ". png", " svgUrl ":" https: \ / \ / sworg \ / images \ / core \ / emoji \ /12.0.0-1 \ / svg \ / "," svgExt ":". svg "," source ": {" concatemoji ":" http: \ / \ / blackaddon.com \ / wp-includes \ / js \ /wp-emoji-release.min.js? ver = 5.3.2 "}};
+			! function (e, a, t) {var r, n, o, i, p = a.createElement ("lienzo"), s = p.getContext && p.getContext ("2d"); función c (e, t) {var a = String.fromCharCode; s.clearRect (0,0, p.width, p.height), s.fillText (a.apply (this, e), 0,0); var r = p.toDataURL ( ); return s.clearRect (0,0, p.width, p.height), s.fillText (a.apply (this, t), 0,0), r === p.toDataURL ()} función l (e) {if (! s ||! s.fillText) return! 1; switch (s.textBaseline = "top", s.font = "600 32px Arial", e) {case "flag": return! c ([127987,65039,8205,9895,65039], [127987,65039,8203,9895,65039]) && (! C ([55356,56826,55356,56819], [55356,56826,8203,55356,56819 ]) &&! c ([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447], [55356,57332,8203,56128,56423,8203,56128 , 56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447])); caso "emoji ": return! c ([55357,56424,55356,57342,8205,55358,56605,8205,55357,56424,55356,57340], [55357,56424,55356,57342,8203,55358,56605,8203, 55357,56424,55356,57340])} return! 1} function d (e) {var t = a.createElement ("script"); t.src = e, t.defer = t.type = "text / javascript ", a.getElementsByTagName (" head ") [0] .appendChild (t)} for (i = Array (" flag "," emoji "), t.supports = {everything:! 0, everythingExceptFlag:! 0}, o = 0; o <i.length; o ++) t.supports [i [o]] = l (i [o]), t.supports.everything = t.supports.everything && t.supports [i [o]], "bandera"! == i [o] && (t.supports.everythingExceptFlag = t.supports.everythingExceptFlag && t.supports [i [o]]); t.supports.everythingExceptFlag = t.supports.everythingExceptFlag &&! t.supports.flag , t.DOMReady =! 1, t.readyCallback = function () {t.DOMReady =! 0}, t.supports.todo || (n = function () {t.readyCallback ()}, a.addEventListener? (a.addEventListener ("DOMContentLoaded", n,! 1), e.addEventListener ("load", n,! 1)) : (e.attachEvent ("onload", n), a.attachEvent ("onreadystatechange", function () {"complete" === a.readyState && t.readyCallback ()})), (r = t.source || {}). concatemoji? d (r.concatemoji): r.wpemoji && r.twemoji && (d (r.twemoji), d (r.wpemoji)))} (ventana, documento, ventana._wpemojiSettings);(d (r.twemoji), d (r.wpemoji)))} (ventana, documento, ventana._wpemojiSettings);(d (r.twemoji), d (r.wpemoji)))} (ventana, documento, ventana._wpemojiSettings);
+		</script>
+		<style type = "text / css">
+img.wp-smiley,
+img.emoji {
+	display: inline! important;
+	borde: ninguno! importante;
+	caja-sombra: ¡ninguna! importante;
+	altura: 1em! importante;
+	ancho: 1em! importante;
+	margen: 0 .07em! importante;
+	alineación vertical: -0.1em! importante;
+	fondo: ninguno! importante;
+	padding: 0! important;
+}
+</style>
+	<link rel = 'stylesheet' id = 'wp-block-library-css' href = 'http: //blackaddon.com/wp-includes/css/dist/block-library/style.min.css? ver = 5.3 .2 'tipo =' texto / css 'media =' todo '/>
+<link rel = 'stylesheet' id = 'coblocks-frontend-css' href = 'http: //blackaddon.com/wp-content/plugins/coblocks/dist/coblocks-style.css? ver = cb804cec39cf5c1f5ba6808500c3d8aa' type = ' text / css 'media =' all '/>
+<link rel = 'stylesheet' id = 'font-awesome-css' href = 'http: //blackaddon.com/wp-content/plugins/contact-widgets/assets/css/font-awesome.min.css? ver = 4.7.0 'tipo =' texto / css 'media =' todo '/>
+<link rel = 'stylesheet' id = 'colormag_google_fonts-css' href = '// fonts.googleapis.com/css?family=Open+Sans%3A400%2C600&ver=5.3.2' type = 'text / css' media = 'todos' />
+<link rel = 'stylesheet' id = 'colormag_style-css' href = 'http: //blackaddon.com/wp-content/themes/colormag/style.css? ver = 5.3.2' type = 'text / css' media = 'todos' />
+<link rel = 'stylesheet' id = 'colormag-fontawesome-css' href = 'http: //blackaddon.com/wp-content/themes/colormag/fontawesome/css/font-awesome.css? ver = 4.2.1 'type =' text / css 'media =' all '/>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-includes/js/jquery/jquery.js? ver = 1.12.4-wp'> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-includes/js/jquery/jquery-migrate.min.js? ver = 1.4.1'> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-content/themes/colormag/js/colormag-custom.js? ver = 5.3.2'> </script>
+<enlace rel = 'https: //api.w.org/' href = 'http: //blackaddon.com/wp-json/' />
+<link rel = "EditURI" type = "application / rsd + xml" title = "RSD" href = "http://blackaddon.com/xmlrpc.php?rsd" />
+<link rel = "wlwmanifest" type = "application / wlwmanifest + xml" href = "http://blackaddon.com/wp-includes/wlwmanifest.xml" /> 
+<meta name = "generator" content = "WordPress 5.3.2" />
+<link rel = "icon" href = "http://blackaddon.com/wp-content/uploads/2020/05/cropped-logo_-03-32x32.png" tamaños = "32x32" />
+<link rel = "icon" href = "http://blackaddon.com/wp-content/uploads/2020/05/cropped-logo_-03-192x192.png" tamaños = "192x192" />
+<link rel = "apple-touch-icon-precomposed" href = "http://blackaddon.com/wp-content/uploads/2020/05/cropped-logo_-03-180x180.png" />
+<meta name = "msapplication-TileImage" content = "http://blackaddon.com/wp-content/uploads/2020/05/cropped-logo_-03-270x270.png" />
+</head>
+
+<body class = "error404 wide">
+<div id = "page" class = "hfeed site">
+		<header id = "masthead" class = "site-header clearfix">
+		<div id = "header-text-nav-container" class = "clearfix">
+         
+			
+			<div class = "envoltura interna">
+
+				<div id = "header-text-nav-wrap" class = "clearfix">
+					<div id = "header-left-section">
+												<div id = "header-text">
+							<h1 id = "site-title">
+								<a href="http://blackaddon.com/" title="Black Addon" rel="home"> Black Addon </a>
+							</h1>
+							<h2 id = "site-description"> Complemento negro para KODI </h2> <! - # site-description ->
+						</div> <! - # header-text ->
+											</div> <! - # header-left-section ->
+					<div id = "header-right-section">
+									    	</div> <! - # header-right-section ->
+
+			   </div> <! - # header-text-nav-wrap ->
+
+			</div> <! - .inner-wrap ->
+
+			
+			<nav id = "site-navigation" class = "main-navigation clearfix" role = "navigation">
+				<div class = "clearfix de envoltura interna">
+					               <h4 class = "menú-alternar"> </h4>
+               <div class = "menu-primary-container"> <ul id = "menu-menu-principal" class = "menu"> <li id ​​= "menu-item-15" class = "menu-item menu-item- type-custom menu-item-object-custom menu-item-home menu-item-15 "> <a href="http://blackaddon.com"> Inicio </a> </li>
+<li id ​​= "menu-item-13" class = "elemento de menú menu-item-type-post_type menu-item-object-page menu-item-13"> <a href = "http://blackaddon.com / sobre-mi / "> Sobre mí </a> </li>
+<li id ​​= "menu-item-12" class = "elemento de menú menu-item-type-post_type menu-item-object-page menu-item-12"> <a href = "http://blackaddon.com / comuniquense-conmigo / "> Comuníquense conmigo </a> </li>
+</ul> </div> </div>
+			</nav>
+
+		</div> <! - # header-text-nav-container ->
+
+		
+	</header>
+			<div id = "main" class = "clearfix">
+		<div class = "clearfix de envoltura interna">
+	
+	<div id = "principal">
+		<div id = "content" class = "clearfix">
+			<section class = "error-404 no encontrado">
+				<div class = "contenido de la página">
+
+											<header class = "page-header">
+							<h1 class = "page-title"> <span> ¡Uups! </span> La página que buscas <span> no se puede </span> encontrar. </h1>
+						</header>
+						<p> Parece que no se encontró nada en este lugar. ¿Tal vez quiera una nueva búsqueda? </p>
+						<form action = "http://blackaddon.com/" class = "formulario de búsqueda formulario de búsqueda clearfix" método = "obtener">
+   <div class = "search-wrap">
+      <input type = "text" placeholder = "Buscar" class = "s field" name = "s">
+      <button class = "search-icon" type = "enviar"> </button>
+   </div>
+</form> <! - .searchform ->					
+				</div> <! - .page-content ->
+			</sección> <! - .error-404 ->
+		</div> <! - #content ->
+	</div> <! - #primary ->
+
+	
+<div id = "secundario">
+			
+				<aside id = "Recent-posts-2" class = "widget widget_recent_entries clearfix"> <h3 class = "widget-title"> <span> Entradas recientes </span> </h3> <ul>
+											<li>
+					<a href="http://blackaddon.com/99-2/"> Instalar Elementum </a>
+									</li>
+											<li>
+					<a href="http://blackaddon.com/como-instalar-f4mtester-en-kodi/"> Cómo instalar F4MTester en Kodi </a>
+									</li>
+											<li>
+					<a href="http://blackaddon.com/instalar-plexus-en-kodi/"> Instalar Plexus en Kodi </a>
+									</li>
+											<li>
+					<a href="http://blackaddon.com/como-descargar-y-configurar-acestream/"> Cómo descargar y configurar AceStream </a>
+									</li>
+											<li>
+					<a href="http://blackaddon.com/instalar-complemento-bg/"> Instalar Complemento BG </a>
+									</li>
+					</ul>
+		</aside>
+	</div>
+	
+
+		</div> <! - .inner-wrap ->
+	</div> <! - #main ->
+   			<footer id = "colofón" class = "clearfix">
+			
+			<div class = "footer-socket-wrapper clearfix">
+				<div class = "envoltura interna">
+					<div class = "footer-socket-area">
+                  <div class = "footer-socket-right-section">
+   						                  </div>
+                  <div class = "footer-socket-left-sectoin">
+   						<div class = "copyright"> Copyright © 2020 <a href="http://blackaddon.com/" title="Black Addon"> <span> Black Addon </span> </a>. Todos los derechos reservados. <br> Tema: ColorMag de <a href="http://themegrill.com/themes/colormag" target="_blank" title="ThemeGrill" rel="designer"> <span> ThemeGrill < / span> </a>. Desarrollado con <a href="http://wordpress.org" target="_blank" title="WordPress"> <span> WordPress </span> </a>. </div> </div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<a href="#masthead" id="scroll-up"> <i class = "fa fa-chevron-up"> </i> </a>
+	</div> <! - #page ->
+	
+<! - Infolinks START ->
+<script type = "text / javascript"> jQuery (documento) .ready (function () {jQuery ('. description'). before ('<span> <! - INFOLINKS_OFF -> </span>'); jQuery ('. descripción'). after ('<span> <! - INFOLINKS_ON -> </span>'); jQuery ('# headerimg'). before ('<span> <! - INFOLINKS_OFF-- > </span> '); jQuery (' # headerimg '). after (' <span> <! - INFOLINKS_ON -> </span> '); jQuery (' h1 '). before (' <span> <!--INFOLINKS_OFF--></span>');jQuery('h1').after('<span><!--INFOLINKS_ON--> </span> '); jQuery (' h1 '). before ('<span> <! - INFOLINKS_OFF -> </span>'); jQuery ('h1'). after ('<span> <! - INFOLINKS_ON -> </span>');} ); </script>
+<script type = "text / javascript">
+    var infolinks_pid = 3267420; 
+    var infolinks_plugin_version = "WP_3.2.3"; 
+    var infolinks_resources = "https://resources.infolinks.com/js"; 
+</script>
+<script type = "text / javascript" src = "https://resources.infolinks.com/js/infolinks_main.js"> </script>
+<! - Infolinks END -> <script type = 'text / javascript' src = 'http: //blackaddon.com/wp-content/plugins/coblocks/dist/js/coblocks-animation.js? Ver = 2.5 .1 '> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-content/themes/colormag/js/jquery.bxslider.min.js? ver = 4.1.2'> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-content/themes/colormag/js/colormag-slider-setting.js? ver = 5.3.2'> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-content/themes/colormag/js/navigation.js? ver = 5.3.2'> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-content/themes/colormag/js/fitvids/jquery.fitvids.js? ver = 20150311'> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-content/themes/colormag/js/fitvids/fitvids-setting.js? ver = 20150311'> </script>
+<script type = 'text / javascript' src = 'http: //blackaddon.com/wp-includes/js/wp-embed.min.js? ver = 5.3.2'> </script>
+</body>
+</html>
